3850a31b3ab0d007de838e24d87bea6097dbf8c7
Vikas_kumar <118371801+vikassinghji@users.noreply.github.com>
2025-02-05 00:57:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT=process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://mern-auth-frontend-uszl.onrender.com/'];
 
 app.use(express.json());
 app.use(cookieParser());
